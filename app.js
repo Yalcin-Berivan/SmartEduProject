44071a11f83c8b2fd689f542ba5a09a0fedb0ce8
Yalcin-Berivan <144229852+Yalcin-Berivan@users.noreply.github.com>
2023-09-27 14:42:38 +0300

    file updated

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const express=require("express");
-const app=express();
-app.get('/',(req,res)=>{
-    res.send('İNDEX SAYFASI')
+const express = require("express");
+const app = express();
+//templete engine
+app.set("view engine", "ejs");
+//middlewares
+app.use(express.static("public"));
+//routes
+app.get("/", (req, res) => {
+  res.status(200).render("about",{
+    page_name:"about"
+  });
+app.get("/", (req, res) => {
+    res.status(200).render("index",{
+        page_name:"index"
+    });
+  });
 });
-const port=3000;
-app.listen(port,()=>{
-    console.log(`App started on port ${port}`)
-})
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
